ec4a9a2dfd56d01f8d034f5c93c93aa274426989
root <root@WIN-KM1TFK5SFQH.localdomain>
2022-10-21 06:12:51 +0300

    Update some stuff

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 echo Install packages
 
-sudo apt-get install -y tmux zsh neovim
+sudo apt-get update && sudo apt-get install -y tmux zsh neovim bpython
 
 cp -r .zshrc .tmux.conf .tmux ~/ 
 
 mkdir ~/.config && cp -r nvim ~/.config/
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 source ~/.zshrc
 
